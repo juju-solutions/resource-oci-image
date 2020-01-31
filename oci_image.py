7898284fa707d79f93f5c74e7a4d99995969b316
Cory Johns <johnsca@gmail.com>
2020-01-31 17:55:29 -0500

    Refactors to better match the framework resource usage pattern and status exceptions

--- oci_image.py
+++ oci_image.py
@@ -1,77 +1,58 @@
 from pathlib import Path
 
 import yaml
-from ops.framework import EventBase, EventSource, EventsBase, Object
-from ops.model import BlockedStatus
-
-
-class OCIImageAvailableEvent(EventBase):
-    def __init__(self, handle, resource_data):
-        super().__init__(handle)
-        self.registry_path = resource_data['registrypath']
-        self.username = resource_data['username']
-        self.password = resource_data['password']
-
-    def snapshot(self):
-        return {
-            'registry_path': self.registry_path,
-            'username': self.username,
-            'password': self.password,
-        }
-
-    def restore(self, snapshot):
-        self.registry_path = snapshot['registry_path']
-        self.username = snapshot['username']
-        self.password = snapshot['password']
-
-
-class OCIImageFailedEvent(EventBase):
-    def __init__(self, handle, status_message):
-        super().__init__(handle)
-        self.status = BlockedStatus(status_message)
-
-    def snapshot(self):
-        return {
-            'message': self.status.message,
-        }
-
-    def restore(self, snapshot):
-        self.status = BlockedStatus(snapshot['message'])
-
-
-class OCIImageEvents(EventsBase):
-    image_available = EventSource(OCIImageAvailableEvent)
-    image_failed = EventSource(OCIImageFailedEvent)
+from ops.framework import Object
+from ops.model import BlockedStatus, ModelError
 
 
 class OCIImageResource(Object):
-    on = OCIImageEvents()
-
     def __init__(self, charm, resource_name):
         super().__init__(charm, resource_name)
         self.resource_name = resource_name
 
-        self.framework.observe(charm.on.start, self.check_resource)
-        self.framework.observe(charm.on.upgrade_charm, self.check_resource)
-
-    def check_resource(self, event):
+    def fetch(self):
         resource_path = self.model.resources.fetch(self.resource_name)
         if not resource_path.exists():
-            # TODO: log
-            # self.model.log(f'Missing file for resource: {self.resource_name}')
-            self.on.image_failed.emit(f'Missing resource: {self.resource_name}')
-            return
+            raise MissingResourceError(self.resource_name)
         resource_text = Path(resource_path).read_text()
         if not resource_text:
-            # TODO: log
-            # self.model.log(f'Empty yaml for resource: {self.resource_name}')
-            self.on.image_failed.emit(f'Missing resource: {self.resource_name}')
-            return
+            raise MissingResourceError(self.resource_name)
         try:
             resource_data = yaml.safe_load(resource_text)
         except yaml.YAMLError as e:
-            # TODO: log
-            # self.model.log(f'Invalid resource yaml {self.resource_name}: {resource_text}')
-            self.on.image_failed.emit(f'Invalid resource: {self.resource_name}')
+            raise InvalidResourceError(self.resource_name)
         else:
-            self.on.image_available.emit(resource_data)
+            return ImageInfo(resource_data)
+
+
+class ImageInfo(dict):
+    def __init__(self, data):
+        # Translate the data from the format used by the charm store to the format
+        # used by the Juju K8s pod spec, since that is how this is typically used.
+        super().__init__({
+            'imagePath': data['registrypath'],
+            'username': data['username'],
+            'password': data['password'],
+        })
+
+    @property
+    def image_path(self):
+        return self['imagePath']
+
+    @property
+    def username(self):
+        return self['username']
+
+    @property
+    def password(self):
+        return self['password']
+
+
+class MissingResourceError(ModelError):
+    def __init__(self, resource_name):
+        super().__init__(resource_name, BlockedStatus(f'Missing resource: {resource_name}'))
+
+
+class InvalidResourceError(ModelError):
+    def __init__(self, resource_name):
+        super().__init__(resource_name, BlockedStatus(f'Invalid resource: {resource_name}'))
